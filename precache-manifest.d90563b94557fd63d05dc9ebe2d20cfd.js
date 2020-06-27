be306da8d8e9a93190d13f6d4cd4e6f82dbd65ca
Albena Kostova <albena.kostov@gmail.com>
2020-06-27 21:51:33 +0200

    Updates

---
+++ precache-manifest.d90563b94557fd63d05dc9ebe2d20cfd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2cf2d428403dab8ea2eeaac8938655e",
+    "url": "/RoboFriends/index.html"
+  },
+  {
+    "revision": "a178ca95338bd53e108f",
+    "url": "/RoboFriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "e59dc12e254d41982293",
+    "url": "/RoboFriends/static/css/main.2144f494.chunk.css"
+  },
+  {
+    "revision": "a178ca95338bd53e108f",
+    "url": "/RoboFriends/static/js/2.87063b01.chunk.js"
+  },
+  {
+    "revision": "6287c65a6963df837d60dc49bb220910",
+    "url": "/RoboFriends/static/js/2.87063b01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e59dc12e254d41982293",
+    "url": "/RoboFriends/static/js/main.b7fa6f39.chunk.js"
+  },
+  {
+    "revision": "671d1b49487ff131bdad",
+    "url": "/RoboFriends/static/js/runtime-main.16920bcc.js"
+  },
+  {
+    "revision": "47c2d629efe58061e3824608410db22f",
+    "url": "/RoboFriends/static/media/EcTunnelRegular-Jxe7.47c2d629.woff"
+  }
+]);
